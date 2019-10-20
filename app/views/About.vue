ccfc2dd3612a1120f93a9bcdc61262613bfb9e48
salmi3d <salmi3d@gmail.com>
2019-10-20 15:43:19 +0300

    added nativescript-vue

---
+++ app/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <Page>
+    <ActionBar :title="navbarTitle"/>
+    <GridLayout>
+      <Label text="This is an about page" textWrap="true" horizontalAlignment="center" verticalAlignment="center"/>
+    </GridLayout>
+  </Page>
+</template>
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component({
+    name: 'about',
+  })
+  export default class About extends Vue {
+    private navbarTitle: string = `About.vue`;
+  }
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import '~styles/style-two';
+  @import '~styles/style-one';
+</style>
